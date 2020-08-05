418f655e4e25f83784ee7767b6a44b582f8ebe3b
Jiayi Ren <jiayiren22@gmail.com>
2020-08-05 14:37:22 -0700

    complete lookup_table

--- applications/lookup_table/lookup_table.py
+++ applications/lookup_table/lookup_table.py
@@ -1,5 +1,9 @@
 # Your code here
+import random
+# import time
+import math
 
+look_up_table = {}
 
 def slowfun_too_slow(x, y):
     v = math.pow(x, y)
@@ -16,11 +20,21 @@ def slowfun(x, y):
     """
     # Your code here
 
+    v = math.pow(x, y)
+    if v not in look_up_table:
+        look_up_table[v] = math.factorial(v)
+        look_up_table[v] //= (x + y)
+        look_up_table[v] %= 982451653
+    v = look_up_table[v]
 
+    return v
 
 # Do not modify below this line!
-
+# start_time = time.time()
 for i in range(50000):
     x = random.randrange(2, 14)
     y = random.randrange(3, 6)
     print(f'{i}: {x},{y}: {slowfun(x, y)}')
+#     slowfun(x,y)
+# end_time = time.time()
+# print(f"runtime: {end_time - start_time} seconds")
