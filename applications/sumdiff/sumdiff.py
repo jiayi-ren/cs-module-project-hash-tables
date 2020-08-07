f796d1a59718c756c93760416593491d942dc0e0
Jiayi Ren <jiayiren22@gmail.com>
2020-08-06 19:49:11 -0700

    refactor while loop using for loop

--- applications/sumdiff/sumdiff.py
+++ applications/sumdiff/sumdiff.py
@@ -22,31 +22,23 @@ def sumdiff(number_set):
 
     # addition dict { f(a)+f(b) : a_b}
     add_comb = {}
-    i = 0
-    while i < len(num_set):
-        j = 0
-        while j < len(num_set)-1:
+    for i in range(len(num_set)):
+        for j in range(len(num_set)-1):
             add_value = fx_lookup[num_set[i]] + fx_lookup[num_set[j]]
             if  add_value not in add_comb:
                 add_comb[add_value] = [f"{num_set[i]}_{num_set[j]}"]
             else:
                 add_comb[add_value].append(f"{num_set[i]}_{num_set[j]}")
-            j += 1
-        i += 1
     
     # subtraction dict {f(c)-f(d) : c_d}
     sub_comb = {}
-    i = len(num_set)-1
-    while i >= 0:
-        j = len(num_set)-1
-        while j >= 0:
+    for i in range(len(num_set)-1, -1, -1):
+        for j in range(len(num_set)-1, -1, -1):
             sub_value = fx_lookup[num_set[i]] - fx_lookup[num_set[j]]
             if sub_value not in sub_comb:
                 sub_comb[sub_value] = [f"{num_set[i]}_{num_set[j]}"]
             else:
                 sub_comb[sub_value].append(f"{num_set[i]}_{num_set[j]}")
-            j -= 1
-        i -= 1
     
     # loop through addition dict
     #  if exits in subtraction dict
@@ -56,11 +48,10 @@ def sumdiff(number_set):
         if value in sub_comb:
             for i in comb:
                 for j in sub_comb[value]:
-                    # pass
                     res.append([i,j])
                     
     # number of all possible combinations
-    print(len(res))
+    # print(len(res))
 
     # print all possible combinations
     for ele in res:
